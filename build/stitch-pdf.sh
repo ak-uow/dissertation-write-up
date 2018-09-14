fab6d72b332d3007e6bb60f60577b2cbeb9774f1
Andrew Keats <w1663560@my.westminster.ac.uk>
2018-09-14 04:44:12 +0100

    I did done a dissertation!!

--- build/stitch-pdf.sh
+++ build/stitch-pdf.sh
@@ -8,11 +8,11 @@ set -e
 out_file="msc-idc_dissertation_w1663560_AndrewKeats"
 data_dir=..
 out_dir=$data_dir/out
-extra_dir=$data_dir/supporting-work
+extra_dir=$data_dir/supporting-work/ux-study
 
 #pdftk file1.pdf file2.pdf cat output mergedfile.pdf
 
-pdftk $out_dir/$out_file $extra_dir/iot-advertising-user-feedback-01.pdf $extra_dir/user-research-analysis.pdf
+pdftk $out_dir/$out_file.pdf $extra_dir/iot-advertising-user-feedback-01.pdf $extra_dir/user-research-analysis.pdf cat output $out_dir/$out_file-all.pdf
 
 if [ $? -eq 0 ]; then
     echo
